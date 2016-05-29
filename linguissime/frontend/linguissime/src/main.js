70afdef0a64552bdbf776c2c237907a11776ddfc
Pixelmod <guillaumedanguin@yahoo.fr>
2016-05-29 19:39:43 +0200

    blabla

--- linguissime/frontend/linguissime/src/main.js
+++ linguissime/frontend/linguissime/src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
 import App from './App'
+import Hello from './components/Hello'
+import Empty from './components/Empty'
+import Navbar from './components/Navbar'
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
-  components: { App }
+  components: {
+    Navbar
+  }
 })
+
+/* eslint-disable no-new */
+var router = new VueRouter()
+
+router.map({
+  '/': {
+    component: App
+  },
+  '/hello': {
+    component: Hello
+  }
+})
+
+router.start(Empty, 'body')
