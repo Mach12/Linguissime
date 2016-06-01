cda6ee2921a4019d02ccae7e86d3aa67e7ebee0e
Pixelmod <guillaumedanguin@yahoo.fr>
2016-06-01 16:11:57 +0200

    Did things...

--- linguissime/frontend/ts/src/default/components/app.ts
+++ linguissime/frontend/ts/src/default/components/app.ts
@@ -1,8 +1,11 @@
 import router from '../router.config';
-
+import navbar from './navbar/navbar';
 
 var app = Vue.extend( {
-    template: "@" //Means that the HTML is located at ./app.html
+    template: "@", //Means that the HTML is located at ./app.html
+    components: {
+        navbar
+    }
 });
 
 //bootstrap the application using the router on the app component
