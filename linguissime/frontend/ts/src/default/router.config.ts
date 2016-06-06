2ee90b1380769ef9d1f17cc41f25b7655b78fc9c
Pixelmod <guillaumedanguin@yahoo.fr>
2016-06-06 11:43:11 +0200

    Removed default components from frontend

--- linguissime/frontend/ts/src/default/router.config.ts
+++ linguissime/frontend/ts/src/default/router.config.ts
@@ -1,21 +1,10 @@
 Vue.use(VueRouter);
-import one      from './components/partone/partone';
-import two      from './components/parttwo/parttwo';
 import exercise from './components/exercise/exercise';
 
 var router = new VueRouter({
     history: true,
 });
 router.map({
-    'partone': {
-        name: 'one',
-        component:one
-        
-    },
-    'parttwo':{
-        name: 'two',
-        component:two
-    },
     'exercise': {
         name: 'exercise',
         component: exercise
