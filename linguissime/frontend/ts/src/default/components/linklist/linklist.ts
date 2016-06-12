52a154abc3dfc6d20f1de3ab5ec241a1c988ecc8
Pixelmod <guillaumedanguin@yahoo.fr>
2016-06-12 16:33:32 +0200

    Linklist is now foldable

--- linguissime/frontend/ts/src/default/components/linklist/linklist.ts
+++ linguissime/frontend/ts/src/default/components/linklist/linklist.ts
@@ -1,10 +1,12 @@
 export default Vue.extend({
     template: "@",
     data: function() {return {
+        show: true,
         links: {
             "Login" : "login",
             "Register" : "register",
-            "Exercise" : "exercise"
+            "Exercise" : "exercise",
+            "Dashboard" : "dashboard"
         }
     }}
 })
