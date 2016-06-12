0fb62ae6d3ee8a9aa58eddead4f8bea236db993a
Pixelmod <guillaumedanguin@yahoo.fr>
2016-06-12 16:33:01 +0200

    Factorized the code for the navbar

--- linguissime/frontend/ts/src/default/components/navbar/navbar.ts
+++ linguissime/frontend/ts/src/default/components/navbar/navbar.ts
@@ -1,4 +1,11 @@
+import connected    from './connected/connected'
+import disconnected from './disconnected/disconnected'
+
 export default Vue.extend({
     template: "@",
-    name: "Navbar"
+    name: "Navbar",
+    components: {
+        connected,
+        disconnected
+    }
 });
