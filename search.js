7cb51e1ceeb189361b8c6e6482b91fdf3018e23b
agrandiere <agrandiere@intechinfo.fr>
2016-06-27 09:54:07 +0200

    script algolia

--- search.js
+++ search.js
@@ -1,6 +1,9 @@
 
 // instantsearch.js - Algolia
 
+
+// config
+
 var search = instantsearch({
   appId: 'xx',
   apiKey: 'xx',
@@ -9,63 +12,32 @@ var search = instantsearch({
 });
 
 
-search.addWidget(
-  instantsearch.widgets.hitsPerPageSelector({
-    container: '#hits-per-page-selector',
-    cssClasses: {
-      root: 'form-control'
-    },
-    options: [
-      {value: 6, label: '6 per page'},
-      {value: 12, label: '12 per page'},
-      {value: 24, label: '24 per page'}
-    ]
-  })
-);
-
+// search input
 
 search.addWidget(
     instantsearch.widgets.searchBox({
-      container: '#search-input'
+        container: '#search-box',
+        placeholder: 'Search for products...'
+      })
+);
+
+// results display in a container
+
+search.addWidget(
+    instantsearch.widgets.hits({
+        container: '#hits-container',
+        templates: {
+            item: 'Hit {{objectID}}: FIXME'
+        }
     })
-  );
-
-search.addWidget(
-  instantsearch.widgets.hits({
-    container: '.hit',
-    templates: {
-      empty: 'No results',
-      item: 'xxxx'
-    },
-    hitsPerPage: 6
-  })
 );
 
-search.addWidget(
-  instantsearch.widgets.pagination({
-    container: '#pagination-container',
-    maxPages: 20,
-    scrollTo: false,
-
-  })
-);
-
+// pagination
 
 search.addWidget(
-  instantsearch.widgets.refinementList({
-    container: '#categories',
-    attributeName: 'category',
-    operator: 'or',
-    cssClasses: {
-      count: 'hidden',
-      active: 'active'
-    },
-    limit: 10,
-
-    templates: {
-      header: 'Catégories'
-    }
-  })
+    instantsearch.widgets.pagination({
+        container: '#pagination-container'
+    })
 );
 
 search.start();
